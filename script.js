9ded8dcec860f5de4210cbdadbc2a933ad731d4e
AshwinGP <111189173+AshwinGP@users.noreply.github.com>
2024-02-26 21:22:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+var arr=[1,2,3,4]
+function promiseMul(){
+	return new Promise((resolve,reject)=>{
+       setTimeout(()=>{
+		let mul=arr.map((element)=>element%2===0? element*2:element);
+		   resolve(mul.join(","));
+	},2000)
+});
+}
+	function promise(){
+	return new Promise((resolve,reject)=>{
+	  setTimeout(()=>{
+		   let oddElements = arr.filter(element => element % 2 !== 0);
+            resolve(oddElements.join(", "));
+	},1000)
+});
+}
+   promise()
+	   .then((res)=>{
+	   document.getElementById("output").textContent=res;
+	   return promiseMul()
+   }).then((res)=>{
+	   document.getElementById("output").textContent=res;
+   }).catch((err)=>console.log("err",err));
+  
+// Promises.all(promise,promiseMUL).then((res)=>{
+// 	document.getElementById("outpt").textContent=res;
+// }).catch((err)=>console.log("err",err));
